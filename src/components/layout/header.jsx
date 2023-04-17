9fbd87b49bc4290e450b2a08ff01cfc6ccf46970
Kantemir Imamov <imamov@skygate.de>
2023-04-17 11:26:56 +0200

    first commit

---
+++ src/components/layout/header.jsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+    return (
+      <header>header content
+        <nav>nav content</nav>
+      </header>
+    )
+  }
---
+++ src/pages/events.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+
+export default function Events() {
+  return (
+    <>
+      <Head>
+        <title>Events Page</title>
+      </Head>
+      <main >
+        <h1>
+          Events Page
+        </h1>
+      </main>
+    </>
+  )
+}
