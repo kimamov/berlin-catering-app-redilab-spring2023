bcbb3cb081376befd236ab19857ab32ab784d2c9
Elina_Usmanova <elina-usmanova-97@mail.ru>
2023-04-25 12:33:18 +0100

    banner image as a background

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,8 +1,16 @@
+import Image from 'next/image';
+import bannerImage from '../images/Fatoush.jpg';
+
+
 export default function HeroSection() {
   return (
     <div>
-      <div className="container">
-        <img src="" alt="" />
+      <div className="container" style={{
+        height: '100vh',        
+        backgroundImage: `url("https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?cs=srgb&dl=pexels-ash-376464.jpg&fm=jpg")`,
+        backgroundSize: 'cover',
+        }}>
+        
         <div className="text-wrapper">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste
           possimus tenetur fugit tempore in provident hic debitis mollitia!
