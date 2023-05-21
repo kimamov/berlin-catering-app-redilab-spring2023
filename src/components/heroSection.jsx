3bd49638a20e5695f51b15e964c4689eca6e9678
Elina_Usmanova <elina-usmanova-97@mail.ru>
2023-05-21 20:37:09 +0100

    add CTA button and link it to the menu page

--- src/components/heroSection.jsx
+++ src/components/heroSection.jsx
@@ -1,14 +1,26 @@
+import styles from "./heroSection.module.css";
+
 export default function HeroSection() {
   return (
     <div>
-      <div className="container">
-        <img src="" alt="" />
+      <div
+        className={styles.container}
+        style={{
+          height: "100vh",
+          backgroundImage: `url("https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?cs=srgb&dl=pexels-ash-376464.jpg&fm=jpg")`,
+          // backgroundImage: `url("../assets/Gemischt.jpg")`,
+          backgroundSize: "cover",
+        }}
+      >
         <div className="text-wrapper">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste
           possimus tenetur fugit tempore in provident hic debitis mollitia!
           Voluptatem soluta sint error dolorum sequi voluptate quod quis nam
           aspernatur! Tempore?
         </div>
+        <a href="/menu" className={styles.ctaButton}>
+          Order now
+        </a>
       </div>
     </div>
   );
