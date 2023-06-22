9c316f1d69e747eb1c4aed8b3b0f5ddf77f4dc57
Younghyun <yhbae97@gmail.com>
2023-06-22 15:15:37 +0200

    add decrease increase quantity btn for each items

--- src/components/Menu/Quantity.jsx
+++ src/components/Menu/Quantity.jsx
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import styles from './Quantity.module.css';
 export default function Quantity({id,setAddtoCart})
 {
-    const{increaseItem,decreaseItem}=useContext(CartContext);
+    const{increaseItem,decreaseItem,removeFromCart}=useContext(CartContext);
     const[value,setValue]=useState(1);
     const handleDecrement=()=>{
         setValue(prevvalue=>prevvalue-1)  
@@ -25,7 +25,9 @@ export default function Quantity({id,setAddtoCart})
     };
     return(
         <div className={styles.button}>
-                <button onClick={handleDecrement}>-</button><span className={styles.cartvalue}>{value}</span><button onClick={handleIncrement}>+</button>   
+            <button onClick={handleDecrement}>-</button>
+            <span className={styles.cartvalue}>{value}</span>
+            <button onClick={handleIncrement}>+</button>   
         </div>
     )
 }
