c0bd76bcf29bf7328aff9baf065581153705e4fe
ako652 <akotabe0213@gmail.com>
2023-09-17 20:34:45 +0200

    responsive cart added/function to check item exist added

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,115 +1,117 @@
-import { createContext, useReducer } from 'react';
-export const CartContext=createContext({items:[],count:0,totalPrice:0});
+import { createContext, useReducer } from "react";
+export const CartContext = createContext({
+  items: [],
+  count: 0,
+  totalPrice: 0,
+});
 //CartContext takes an array of items as the default value.
-export const CartProvider=({children})=>{
-    const[state,dispatch]=useReducer(Reducer,{items:[],count:0,totalPrice:0,});
-    const SumOfItems=(cartitems)=>{
-        let sum=cartitems.reduce((total,product)=>total+product.quantity,0);
-        console.log("state.count"+state.count);
-        return sum; 
-    }
-  
-    //addToCart function appends the new product to the existing products and returns the updated products in the payload object of the dispatch function.
-    const addToCart=(product)=>{
-        console.log(product);
-        const updatedCart=[...state.items,product];
-        dispatch(
-            {
-                type:"ADD",
-                payload:{
-                    items:updatedCart,
-                    sum:SumOfItems(updatedCart)
-                }
-            }
-        )
-        console.log(state.items);
-    }
-    const removeFromCart=(id)=>{
-        const updatedCart=state.items.filter(
-            (currentProduct)=>currentProduct.id!==id
-        ); 
-        dispatch(
-            {
-                type:"DEL",
-                payload:{
-                    items:updatedCart,
-                    sum:SumOfItems(updatedCart)
-                }
-            }
-        )
-    }
-    const increaseItem=(id,value)=>{
-        (state.items.find(currentProduct=>currentProduct.id==id)).quantity++;
-       dispatch(
-        {
-            type:"INCREASE",
-            payload:{
-                items:state.items,
-                sum: SumOfItems(state.items)
-            }
-        }
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, {
+    items: [],
+    count: 0,
+    totalPrice: 0,
+  });
+  const SumOfItems = (cartitems) => {
+    let sum = cartitems.reduce((total, product) => total + product.quantity, 0);
+    console.log("state.count" + state.count);
+    return sum;
+  };
 
-       )   
+  //addToCart function appends the new product to the existing products and returns the updated products in the payload object of the dispatch function.
+  const addToCart = (product) => {
+    console.log(product);
+    const existingProduct = state.items.find((item) => item.id === product.id);
+    if (!existingProduct) {
+      const updatedCart = [...state.items, product];
+      dispatch({
+        type: "ADD",
+        payload: {
+          items: updatedCart,
+          sum: SumOfItems(updatedCart),
+        },
+      });
+      console.log(state.items);
     }
-    const decreaseItem=(id,value)=>{
-        state.items.find(currentProduct=>currentProduct.id==id).quantity--;
-       
-       dispatch(
-        {
-            type:"DECREASE",
-            payload:{
-                items:state.items,
-                sum:SumOfItems(state.items)
-            }
-        }
-       )
-    }
-    const value={
-        items:state.items,
-        addToCart,
-        removeFromCart,
-        increaseItem,
-        decreaseItem,
-        SumOfItems,
-        count:state.count
-    }
-    return <CartContext.Provider value={value}>{children}</CartContext.Provider>
-}
-const Reducer=(state,action)=>{
-    const{type,payload}=action;
-    switch(type){
-        case "ADD":
-                return{
-                    ...state,
-                    items:payload.items,
-                    count:payload.sum
-                };
-        case "DEL":
-            return{
-                ...state,
-                items:payload.items,
-                count:payload.sum
-            };
-            case "INCREASE":
-                 console.log("increase"+payload.sum);
-                 console.log({
-                    ...state,
-                    items:payload.items,
-                    count:payload.sum
-                })
-                return{
-                    ...state,
-                    items:payload.items,
-                    count:payload.sum
-                };
-            case "DECREASE":
-                return{
-                    ...state,
-                    items:payload.items,
-                    count:payload.sum
-                };
-default:
-    throw new Error("Error case");
-    }
+  };
 
-}
+  const removeFromCart = (id) => {
+    const updatedCart = state.items.filter(
+      (currentProduct) => currentProduct.id !== id
+    );
+    dispatch({
+      type: "DEL",
+      payload: {
+        items: updatedCart,
+        sum: SumOfItems(updatedCart),
+      },
+    });
+  };
+  const increaseItem = (id, value) => {
+    state.items.find((currentProduct) => currentProduct.id == id).quantity++;
+    dispatch({
+      type: "INCREASE",
+      payload: {
+        items: state.items,
+        sum: SumOfItems(state.items),
+      },
+    });
+  };
+  const decreaseItem = (id, value) => {
+    state.items.find((currentProduct) => currentProduct.id == id).quantity--;
+
+    dispatch({
+      type: "DECREASE",
+      payload: {
+        items: state.items,
+        sum: SumOfItems(state.items),
+      },
+    });
+  };
+  const value = {
+    items: state.items,
+    addToCart,
+    removeFromCart,
+    increaseItem,
+    decreaseItem,
+    SumOfItems,
+    count: state.count,
+  };
+  return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
+};
+const Reducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case "ADD":
+      return {
+        ...state,
+        items: payload.items,
+        count: payload.sum,
+      };
+    case "DEL":
+      return {
+        ...state,
+        items: payload.items,
+        count: payload.sum,
+      };
+    case "INCREASE":
+      console.log("increase" + payload.sum);
+      console.log({
+        ...state,
+        items: payload.items,
+        count: payload.sum,
+      });
+      return {
+        ...state,
+        items: payload.items,
+        count: payload.sum,
+      };
+    case "DECREASE":
+      return {
+        ...state,
+        items: payload.items,
+        count: payload.sum,
+      };
+    default:
+      throw new Error("Error case");
+  }
+};
